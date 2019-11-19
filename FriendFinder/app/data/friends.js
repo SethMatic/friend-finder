81c9125a5f3e43c9e0a57c0500eaa48da8506c34
Seth Dousman <sdousman3@gmail.com>
2019-11-19 17:15:30 -0500

    finished HTML and routing paths

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,41 @@
+var friends = [
+    {
+        name:'Seth Matic',
+        photo:
+        'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj_1PPUo_flAhWqVt8KHRdnBKIQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.imdb.com%252Fname%252Fnm5731353%252F%26psig%3DAOvVaw09TwUpamjmn95efPf-unFh%26ust%3D1574287384490499&psig=AOvVaw09TwUpamjmn95efPf-unFh&ust=1574287384490499',
+        scores:[5, 3, 4, 4, 5, 1, 2, 5, 4, 1]
+     },
+     {
+        name:'Rhianna',
+        photo:
+        'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj_1PPUo_flAhWqVt8KHRdnBKIQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.imdb.com%252Fname%252Fnm5731353%252F%26psig%3DAOvVaw09TwUpamjmn95efPf-unFh%26ust%3D1574287384490499&psig=AOvVaw09TwUpamjmn95efPf-unFh&ust=1574287384490499',
+        scores:[3, 2, 5, 1, 1, 1, 4, 5, 4, 4]
+     },
+     {
+        name:'Gucci Mane',
+        photo:
+        'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj_1PPUo_flAhWqVt8KHRdnBKIQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.imdb.com%252Fname%252Fnm5731353%252F%26psig%3DAOvVaw09TwUpamjmn95efPf-unFh%26ust%3D1574287384490499&psig=AOvVaw09TwUpamjmn95efPf-unFh&ust=1574287384490499',
+        scores:[2, 4, 4, 3, 2, 1, 2, 5, 4, 2]
+     },
+     {
+        name:'Michael Jordan',
+        photo:
+        'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj_1PPUo_flAhWqVt8KHRdnBKIQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.imdb.com%252Fname%252Fnm5731353%252F%26psig%3DAOvVaw09TwUpamjmn95efPf-unFh%26ust%3D1574287384490499&psig=AOvVaw09TwUpamjmn95efPf-unFh&ust=1574287384490499',
+        scores:[1, 2, 4, 4, 5, 5, 4, 5, 3, 3]
+     },
+     {
+        name:'2 Chainz',
+        photo:
+        'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj_1PPUo_flAhWqVt8KHRdnBKIQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.imdb.com%252Fname%252Fnm5731353%252F%26psig%3DAOvVaw09TwUpamjmn95efPf-unFh%26ust%3D1574287384490499&psig=AOvVaw09TwUpamjmn95efPf-unFh&ust=1574287384490499',
+        scores:[5, 5, 4, 4, 5, 1, 5, 3, 4, 4]
+     },
+     {
+        name:'Bart Simpson',
+        photo:
+        'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj_1PPUo_flAhWqVt8KHRdnBKIQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.imdb.com%252Fname%252Fnm5731353%252F%26psig%3DAOvVaw09TwUpamjmn95efPf-unFh%26ust%3D1574287384490499&psig=AOvVaw09TwUpamjmn95efPf-unFh&ust=1574287384490499',
+        scores:[4, 3, 1, 2, 2, 1, 2, 3, 4, 5]
+     },
+ ];
+
+ module.exports = friends;
+
